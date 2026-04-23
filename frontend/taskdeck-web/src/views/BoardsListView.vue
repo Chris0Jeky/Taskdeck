47b6fa12cdb39957fa922591745f2e63b33eafdb
Chris0Jeky <jeky.tck@gmail.com>
2026-04-24 00:41:57 +0100

    Sanitize console.error in BoardsListView via logError
    Part of #958

--- frontend/taskdeck-web/src/views/BoardsListView.vue
+++ frontend/taskdeck-web/src/views/BoardsListView.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useBoardStore } from '../store/boardStore'
+import { logError } from '../utils/errorReporting'
 
 const router = useRouter()
 const boardStore = useBoardStore()
@@ -30,7 +31,7 @@ async function createBoard() {
     // Navigate to the new board
     router.push(`/boards/${board.id}`)
   } catch (error) {
-    console.error('Failed to create board:', error)
+    logError('Failed to create board:', error)
   }
 }
 
