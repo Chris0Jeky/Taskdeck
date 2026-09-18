b8327d8de145b3f72cce34c8450a2978f7f7f3f4
Cristian Tcaci <59696583+Chris0Jeky@users.noreply.github.com>
2026-09-18 16:56:08 +0100

    test(api): restore FluentAssertions import

--- backend/tests/Taskdeck.Api.Tests/TypedRelationProposalAdmissionInputSafetyApiTests.cs
+++ backend/tests/Taskdeck.Api.Tests/TypedRelationProposalAdmissionInputSafetyApiTests.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Http.Json;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Taskdeck.Api.Tests.Support;
